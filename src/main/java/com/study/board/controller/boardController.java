3f2dffcc6e4bdd23d5a9ccc6439479f9121c3db9
yeawonbong <ybong.develop@gmail.com>
2022-01-30 18:42:23 +0900

    게시물 수정기능 추가

--- src/main/java/com/study/board/controller/boardController.java
+++ src/main/java/com/study/board/controller/boardController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -25,12 +26,42 @@ public class boardController {
     public String boardWriteProcess(Board board) {
 
         boardService.write(board);
-        return "/board";
+        return "redirect:/board/list";
     }
 
     @GetMapping("/board/list")
     public String boardList(Model model) { // [spring] 데이터를 담아서 페이지로 보내줄 때 쓰는 Model
         model.addAttribute("list", boardService.boardList()); // 2인자를 1이름을 붙여 넘기겠다는 표현
         return "boardlist";
+    }
+
+    @GetMapping("/board/view") // localhost:8080/board/view?id=1 파라미터 get방식
+    public String boardView(Model model, Integer id) {
+
+        model.addAttribute("board", boardService.boardView(id));
+        return "boardview";
+    }
+
+    @GetMapping("/board/delete")
+    public String boardDelete(Integer id) {
+        boardService.boardDelete(id);
+        return "redirect:/board/list"; // (redirect, 나중에 찾아볼것)
+    }
+
+    @GetMapping("/board/modify/{id}") //path variable
+    public String boardModify(@PathVariable("id") Integer id,
+                              Model model) {
+        model.addAttribute("board", boardService.boardView(id));
+        return "boardmodify";
+    }
+
+    @PostMapping("/board/update/{id}")
+    public  String boardUpdate(@PathVariable("id") Integer id, Board board) {
+        Board boardTemp = boardService.boardView(id);
+        boardTemp.setTitle(board.getTitle());     //인자로 들어온 값 덮어씌우기
+        boardTemp.setContent(board.getContent());
+
+        boardService.write(boardTemp); // 덮어씌우기
+        return "redirect:/board/list";
     }
 }
--- src/main/java/com/study/board/service/BoardService.java
+++ src/main/java/com/study/board/service/BoardService.java
@@ -13,15 +13,30 @@ public class BoardService {
     @Autowired // [Spring] 객체 생성 안해도, spring bean이 알아서 읽어와서 주입을 해준다. dependency injection
     private BoardRepository boardRepository;
 
+    // 작성된 글 처리
     public void write(Board board) {
 
         boardRepository.save(board);
 
     }
 
+    // 게시물 리스트 불러오기 처리
     public List<Board> boardList() {
 
         return boardRepository.findAll(); // list<Board>를 반환
 
     }
+
+    // 특정 게시물 불러오기 처리
+    public Board boardView(Integer id) {
+
+        return boardRepository.findById(id).get();
+    }
+
+    // 특정 게시물 삭제
+    public void boardDelete(Integer id) {
+        boardRepository.deleteById(id);
+    }
+
+
 }
